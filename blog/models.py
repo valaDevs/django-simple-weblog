494c2a46bd3989289a54aecfeb67bffb6b1726a5
vala <valanerd77@gmail.com>
2023-01-15 23:03:16 +0330

    blogfa v1.0

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    cover = models.ImageField(upload_to='images', default='https://img.icons8.com/ios/512/image.png')
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
---
+++ blogfa/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/',views.about),
+    path('', include('blog.urls')),
+   
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
